833c6bd25ac1e97f4ed5c8cc3b4746093d3ce5e8
Daniel <Dannyhan8698@gmail.com>
2020-01-19 02:07:44 -0700

    ..

--- PythonAPI.py
+++ PythonAPI.py
@@ -17,48 +17,51 @@ def api_id():
     #Collections
     riding = provincial["Riding"]
 
+    #User Input from WEB
+    user = input()
+
     #Query [Unemployment Rate] in terms of PERCENTAGE
     #Users can define percentage which will then be counted as input
-    riding.find({"Unemployment Rate":{$gte: $USERINPUT}})
+    riding.find({"Unemployment Rate":{$gte:user}})
 
     #Query [Household Income] in terms of REAL INT
     #Users can define percentage which will then be counted as input
-    riding.find({"Household Income":{$gte:$USERINPUT ==> ?}})
+    riding.find({"Household Income":{$gte:user}})
 
     #Query [Age Group] in terms of INT RANGE
     #Range will be pre-determined and users can select from a set of age ranges
-    riding.find({"Age [20-34]":$USERINPUT})
-    riding.find({"Age [35-49]":$USERINPUT})
-    riding.find({"Age [50-64]":$USERINPUT})
-    riding.find({"Age [65+]":$USERINPUT})
+    riding.find({"Age [20-34]":user})
+    riding.find({"Age [35-49]":user})
+    riding.find({"Age [50-64]":user})
+    riding.find({"Age [65+]":user})
 
     #Query [Immigrants] in terms of PERCENTAGE
     #Users can define percentage which will then be counted as input
-    riding.find({"Immigrants":{$gte:$USERINPUT ==> ?}})
+    riding.find({"Immigrants":{$gte:user}})
 
     #Query [Ethnicity] in terms of PERCENTAGE
     #Users can define percentage which will then be counted as input
-    riding.find({"White":{$gte:$USERINPUT ==> ?}})
-    riding.find({"Asian":{$gte:$USERINPUT ==> ?}})
-    riding.find({"African":{$gte:$USERINPUT ==> ?}})
-    riding.find({"Hispanic":{$gte:$USERINPUT ==> ?}})
-    riding.find({"Middle Eastern":{$gte:$USERINPUT ==> ?}})
+    riding.find({"White":{$gte:user}})
+    riding.find({"Asian":{$gte:user}})
+    riding.find({"African":{$gte:user}})
+    riding.find({"Hispanic":{$gte:user}})
+    riding.find({"Middle Eastern":{$gte:user}})
 
     #Query [Education Level] in terms of 4 DISTINCT CATEGORIES
     #1) NO CERTIFICATE 2) HIGH SCHOOL DIPLOMA 3) Post-Secondary
     #Education levels will be pre-determined and users can select from a set of categories
 
-    riding.find({"Education Level":$USERINPUT})
-    riding.find({"Education Level":$USERINPUT})
-    riding.find({"Education Level":$USERINPUT})
+    riding.find({"Education Level":user})
+    riding.find({"Education Level":user})
+    riding.find({"Education Level":user})
 
     #Query [Historical] in terms of DISTRICT
     #Lists the previous 5 parties voted in a specific DISTRICT
-    riding.find({"Historical [2003]":{"DISTRICT":{$USERINPUT ==> ?}}})
-    riding.find({"Historical [2007]":{"DISTRICT":{$USERINPUT ==> ?}}})
-    riding.find({"Historical [2011]":{"DISTRICT":{$USERINPUT ==> ?}}})
-    riding.find({"Historical [2015]":{"DISTRICT":{$USERINPUT ==> ?}}})
-    riding.find({"Historical [2019]":{"DISTRICT":{$USERINPUT ==> ?}}})
+    riding.find({"Historical [2003]":user})
+    riding.find({"Historical [2007]":user})
+    riding.find({"Historical [2011]":user})
+    riding.find({"Historical [2015]":user})
+    riding.find({"Historical [2019]":user})
 
     return jsonify(results)
 
